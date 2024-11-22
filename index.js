fed997f9eafe18401aed75f6b2861df9680010aa
Quinnmitch45 <quinnmitchell565@gmail.com>
2024-11-22 15:08:22 +1100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Customize your bot's status by forking this."];
+const statusMessages = ["Playing On your Fortnite account"];
 
 
 let currentIndex = 0;
